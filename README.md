b5b183c4622bae4679550a4605b8b85a5853475f
vanerk03 <vanerk4@gmail.com>
2023-07-11 00:33:27 +0300

    0.0.6 add code to the .py files + minor fixes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
 References:
 1. https://arxiv.org/abs/1611.07004
-2. https://machinelearningmastery.com/how-to-develop-a-conditional-generative-adversarial-network-from-scratch/
-3. https://arxiv.org/abs/1411.1784
+2. https://arxiv.org/abs/1411.1784
+3. https://machinelearningmastery.com/how-to-develop-a-conditional-generative-adversarial-network-from-scratch/
 4. https://d2l.ai/chapter_generative-adversarial-networks/index.html
 5. https://www.youtube.com/watch?v=banZhpreS2Y&list=PLEwK9wdS5g0onnKgvKxuUJN1Ojchl9Q9P&index=22
 6. https://github.com/soumith/ganhacks
@@ -11,11 +11,18 @@ References:
 9. https://www.kaggle.com/code/kmldas/mnist-generative-adverserial-networks-in-pytorch
 10. http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/
 
+### Conditional GAN on MNIST dataset
+![Conditioned sample](images/cgan_MNIST.png)
+Parameters:
+50 Epoches, Discriminator and Generator are both of MLP architecture
+Trained with Adam + Adam, lr = 3e-4 (hands down the best one for Adam)
+#### TODO ADD ARCHITECTURE IMAGE
+
 ### How Conditional GAN embdeddings work
 There are many ways to encode and incorporate the class labels into the discriminator and generator models. A best practice involves using an embedding layer followed by a fully connected layer with a linear activation that scales the embedding to the size of the image before concatenating it in the model as an additional channel or feature map.
 
 A version of this recommendation was described in the 2015 paper titled “Deep Generative Image Models using a Laplacian Pyramid of Adversarial Networks.”
 
-… we also explore a class conditional version of the model, where a vector c encodes the label. This is integrated into Gk & Dk by passing it through a linear layer whose output is reshaped into a single plane feature map which is then concatenated with the 1st layer maps.
+We also explore a class conditional version of the model, where a vector c encodes the label. This is integrated into Gk & Dk by passing it through a linear layer whose output is reshaped into a single plane feature map which is then concatenated with the 1st layer maps.
 
-— Deep Generative Image Models using a Laplacian Pyramid of Adversarial Networks, 2015.
+(c) Deep Generative Image Models using a Laplacian Pyramid of Adversarial Networks, 2015.
--- cgan.py
+++ cgan.py
@@ -1,99 +1,74 @@
 from torch import nn
 import torch
-import torchvision.transforms as T
 import matplotlib.pyplot as plt
-import tqdm
-
-
-class Reshape(torch.nn.Module):
-    @property
-    def device(self):
-        return next(self.parameters()).device
-
-    def __init__(self, *args):
-        super().__init__()
-        self.dims = args
-
-    def forward(self, input):
-        return input.view(input.size(0), *self.dims)
+import torchvision
+import torchvision.transforms as T
+import sys
+import platform
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+import numpy as np
 
 
 class G_model(nn.Module):
-    def __init__(self, device):
+    def __init__(self):
         super().__init__()
-        self.device = device
-        self.conditional = nn.Sequential(
-            nn.Linear(50, 49),
-            Reshape(1, 7, 7)
-        )
 
-        self.reshape_noise = nn.Sequential(
-            nn.Linear(100, 6272),
-            nn.LeakyReLU(0.2),
-            Reshape(128, 7, 7),
-        )
+        self.embed = nn.Embedding(10, 10)
 
         self.model = nn.Sequential(
-            nn.ConvTranspose2d(129, 128, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(128, 1, kernel_size=7, stride=1, padding=3),
-            nn.Tanh(),
+            nn.Linear(100 + 10, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 28 * 28),
+            nn.Tanh()
         )
 
-    def forward(self, X, y):
-        # create embedding for y
-        ohe = torch.eye(50, device=self.device)[y]
-        embed = self.conditional(ohe)
-        noise = self.reshape_noise(X)
-        return self.model(torch.cat((embed, noise), dim=1))
-
+    def forward(self, X, labels):
+        emb = self.embed(labels)
+        return self.model(torch.cat((X, emb), dim=1)).view(-1, 28, 28)
 
 class D_model(nn.Module):
-    def __init__(self, n_classes, device):
+    def __init__(self):
         super().__init__()
-        self.device = device
-        self.conditional = nn.Sequential(
-            nn.Linear(50, 784),
-            Reshape(1, 28, 28)
-        )
+
+        self.embed = nn.Embedding(10, 10)
 
         self.model = nn.Sequential(
-            # two in_channels after concat
-            nn.Conv2d(in_channels=2, out_channels=128,
-                      kernel_size=2, stride=2),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128,
-                      kernel_size=2, stride=2),
-            nn.LeakyReLU(),
-            nn.Flatten(start_dim=1),
-            nn.Dropout(p=0.5),
-            nn.Linear(6272, n_classes)
+            nn.Linear(784 + 10, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 28 * 28),
+            nn.LeakyReLU(0.2),
+            nn.Linear(28 * 28, 1),
+            nn.LeakyReLU(0.2),
         )
 
-    def forward(self, X, y):
-        # create embedding for y
-        ohe = torch.eye(50, device=self.device)[y]
-        embed = self.conditional(ohe)
-        return self.model(torch.cat((embed, X), dim=1))
 
+    def forward(self, X, labels):
+        emb = self.embed(labels)
+        X_ = X.view(len(labels), -1)
+        return self.model(torch.cat((X_, emb), dim=1))
 
 class CGAN(nn.Module):
     def __init__(self, device):
         super().__init__()
         self.device = device
         # terrible thing with device, mb this this
-        self.G = G_model(device).to(device)
-        self.D = D_model(1, device).to(device)
+        self.G = G_model().to(device)
+        self.D = D_model().to(device)
 
-    def forward(self, y):
-        noise = torch.randn((len(y), 100), device=self.device)
-        return self.G(noise)
+    def forward(self, n, labels=None):
+        if labels is None:
+            labels = torch.randint(0, 10, (n,)).to(self.device)
+        labels = labels.to(self.device)
+        noise = torch.randn((n, 100), device=self.device)
+        return self.G(noise, labels)
 
-    def train(self, dataloader, EPOCHS, verbose=True):
-        noise_fixed = torch.randn((5, 100), device=self.device)
-
+    def train_(self, dataloader, EPOCHS=100, verbose=True):
+        num_imgs = 10
+        noise_fixed = torch.randn((num_imgs, 100), device=self.device)
+        labels_fixed = torch.arange(10).to(self.device)
 
         criterion = nn.BCEWithLogitsLoss()
 
@@ -106,87 +81,85 @@ class CGAN(nn.Module):
         batch_size = dataloader.batch_size
         G_loss_lst, D_loss_lst = [], []
 
-        for epoch in tqdm.notebook.tqdm(range(EPOCHS), desc="Epoch"):
-
-            G_running_loss = 0
-            D_running_loss = 0
-            for i, (X, y) in enumerate(tqdm.notebook.tqdm(dataloader, leave=False, desc=f"Epoch {epoch}")):
+        for epoch in tqdm(range(EPOCHS), desc="Epoch"):
+            G_running_loss_lst = []
+            D_running_loss_lst = []
+            for i, (X, labels) in enumerate(tqdm(dataloader, leave=False, desc=f"Epoch {epoch}")):
                 # Label Smoothing,
                 # i.e. if you have two target labels:
                 # Real=1 and Fake=0, then for each incoming sample, if it is real,
                 # then replace the label with a random number between 0.7 and 1.2,
                 # and if it is a fake sample, replace it with 0.0 and 0.3 (for example).
                 # Salimans et. al. 2016
-                ones = (0.7 - 1.2) * torch.rand(batch_size,
-                                                device=self.device) + 1.2
-                zeros = 0.3 * torch.rand(batch_size, device=self.device)
+                # ones = (0.7 - 1.2) * torch.rand(batch_size,
+                                                # device=self.device) + 1.2
+                # zeros = 0.3 * torch.rand(batch_size, device=self.device)
 
-                noise = torch.randn(size=(batch_size, 100), device=self.device)
+
+                ones = torch.ones(batch_size, device=self.device)
+                zeros = torch.zeros(batch_size, device=self.device)
+
+                noise = torch.randn((batch_size, 100), device=self.device)
+                labels = labels.to(self.device)
+
                 # iteration for discriminator
-                z = self.G(noise, y)
+                z = self.G(noise, labels)
 
-                pred_fake = self.D(z.detach(), y).squeeze(-1)
-                pred_real = self.D(X.to(self.device), y).squeeze(-1)
+                pred_fake = self.D(z.detach(), labels).squeeze(-1)
+                pred_real = self.D(X.to(self.device), labels).squeeze(-1)
 
                 D_loss = criterion(pred_fake, zeros) + \
                     criterion(pred_real, ones)
 
                 D_loss.backward()
                 optimizer_D.step()
-                D_running_loss += D_loss.item() / batch_size
+                D_running_loss_lst.append(D_loss.item())
 
                 # iteration for the generator
-                pred = self.D(z, y).squeeze(-1)
+                pred = self.D(z, labels).squeeze(-1)
                 G_loss = criterion(pred, ones)
 
                 G_loss.backward()
                 optimizer_G.step()
-                G_running_loss += G_loss.item() / batch_size
+                G_running_loss_lst.append(G_loss.item())
 
                 optimizer_D.zero_grad()
                 optimizer_G.zero_grad()
 
+
                 # fix noise + logging
-                if verbose and (i + 1) % 500 == 0:
+                if (i + 1) % 500 == 0:
+                    self.eval()
                     with torch.no_grad():
-                        print(
-                            f"Epoch {_+1}, batch {i+1}, D_loss: {D_loss.item()}, G_loss: {G_loss.item()}")
-                        num_imgs = 5  # Adjust the number of columns as per your preference
                         # Adjust the figsize as per your preference
-                        _, axs = plt.subplots(1, num_imgs, figsize=(15, 5))
+                        _, axs = plt.subplots(1, num_imgs, figsize=(17, 6))
 
                         # imgs = self.G(noise_fixed)
-                        imgs = self.G(noise_fixed)
-
+                        imgs = self.G(noise_fixed, labels_fixed)
                         for j, img in enumerate(imgs):
                             axs[j].imshow(
-                                img.cpu().detach().numpy().squeeze(), cmap="gray")
+                                undo_transform(img.cpu().detach().view(1, 28, 28)).view(28, 28).numpy(), cmap="gray")
+
+                        plt.show()
+                        self.train()
+
+                        print("G_loss:", np.mean(G_running_loss_lst), "D_loss:", np.mean(D_running_loss_lst))
+                        
+                        plt.plot(torch.tensor(G_running_loss_lst).view(-1, 10).mean(1), label='G-loss')
+                        plt.plot(torch.tensor(D_running_loss_lst).view(-1, 10).mean(1), label='D-loss')
+                        plt.legend(loc='upper right')
 
                         plt.show()
 
-            G_loss_lst.append(G_running_loss)
-            D_loss_lst.append(D_running_loss)
+            G_loss_lst.append(np.mean(G_running_loss_lst))
+            D_loss_lst.append(np.mean(D_running_loss_lst))
 
-            print(G_running_loss, D_running_loss)
+            print("G_loss:", np.mean(G_running_loss_lst), "D_loss:", np.mean(D_running_loss_lst))
+            
+            plt.plot(G_running_loss_lst, label='G-loss')
+            plt.plot(D_running_loss_lst, label='D-loss')
+            plt.legend(loc='upper right')
+
+            plt.show()
 
         return G_loss_lst, D_loss_lst
-
-    def D_step(self, X, y):
-        pass
-
-    def G_step(self, X, y):
-        pass
-
-
-if __name__ == "__main__":
-    device = "mps:0" if getattr(torch, 'has_mps', False) \
-        else "cuda:0" if torch.cuda.is_available() else "cpu"
-
-    model = G_model(device)
-    X = torch.randn((32, 100))
-    y = torch.randint(0, 9, (32,))
-
-    z = model(X, y)
-    discriminator = D_model(1, device)
-
-    print(discriminator(z, y).shape)
